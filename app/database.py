1effc550e6c2693de37ab4dff9955f7fb55e0e31
Людмила Брсукова <ludmilabarsukova13@gmail.com>
2026-06-02 16:26:55 +0300

    1

--- app/database.py
+++ app/database.py
@@ -1,1 +1,27 @@
-# database connection will be here
+# Файл отвечает за соединение с PostgreSQL
+from sqlalchemy.ext.asyncio import (
+    AsyncSession,
+    async_sessionmaker,
+    create_async_engine,
+)
+from sqlalchemy.orm import DeclarativeBase
+
+from app.config import settings
+
+
+engine = create_async_engine(settings.database_url, echo=True)
+
+async_session_maker = async_sessionmaker(
+    engine,
+    class_=AsyncSession,
+    expire_on_commit=False,
+)
+
+
+class Base(DeclarativeBase):
+    pass
+
+
+async def get_async_session():
+    async with async_session_maker() as session:
+        yield session
--- app/main.py
+++ app/main.py
@@ -1,6 +1,12 @@
 from fastapi import FastAPI
-app = FastAPI(title='Async Payment API')
 
-@app.get('/')
+app = FastAPI(
+    title="Async Payment API",
+    description="REST API for users, accounts, payments and webhook processing",
+    version="1.0.0",
+)
+
+
+@app.get("/")
 async def root():
-    return {'status':'ok'}
+    return {"status": "ok"}
