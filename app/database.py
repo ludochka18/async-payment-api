0e38e0684ef4916773d30cd7a9386a0d1783ca89
Людмила Брсукова <ludmilabarsukova13@gmail.com>
2026-06-02 16:46:26 +0300

    1

--- app/database.py
+++ app/database.py
@@ -9,19 +9,19 @@ from sqlalchemy.orm import DeclarativeBase
 from app.config import settings
 
 
-engine = create_async_engine(settings.database_url, echo=True)
+engine = create_async_engine(settings.database_url, echo=True) # подключение к базе
 
-async_session_maker = async_sessionmaker(
+async_session_maker = async_sessionmaker( # создаёт сессию для запросов
     engine,
     class_=AsyncSession,
     expire_on_commit=False,
 )
 
 
-class Base(DeclarativeBase):
+class Base(DeclarativeBase): # базовый класс для всех моделей
     pass
 
 
-async def get_async_session():
+async def get_async_session(): # функция, которую FastAPI будет использовать в роутах
     async with async_session_maker() as session:
         yield session
